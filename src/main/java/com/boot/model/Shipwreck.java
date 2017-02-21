aad427953d5d774a2ca8e91a2287da464d64a47a
laushinka <laurie.malau@gmail.com>
2017-02-21 20:46:07 +0100

    Add Shipwreck class and constructor

---
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -0,0 +1,31 @@
+package com.boot.model;
+
+public class Shipwreck {
+
+    public class Shipwreck {
+        Long id;
+        String name;
+        String description;
+        String condition;
+        Integer depth;
+        Double latitude;
+        Double longitude;
+        Integer yearDiscovered;
+    }
+
+    public Shipwreck() {
+    }
+
+    public Shipwreck(Long id, String name, String description, String condition, Integer depth,
+                     Double latitude, Double longitude, Integer yearDiscovered) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.condition= condition;
+        this.depth = depth;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.yearDiscovered = yearDiscovered;
+    }
+
+}
