5d9af7b5c496259612dedc3db0ac7fa5021bbe10
laushinka <laurie.malau@gmail.com>
2017-02-21 20:58:12 +0100

    Add stub

--- src/main/java/com/boot/controller/ShipwreckController.java
+++ src/main/java/com/boot/controller/ShipwreckController.java
@@ -3,6 +3,9 @@ package com.boot.controller;
 import com.boot.model.Shipwreck;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
@@ -11,6 +14,6 @@ public class ShipwreckController {
 
     @RequestMapping(value = "shipwreck", method = GET)
     public List<Shipwreck> list() {
-
+        return ShipwreckStub.list();
     }
 }
